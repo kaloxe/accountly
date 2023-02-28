0aad0c6094c8e580c7bc4e479300f76717855a0d
casm24 <sanchezcarloscasm@gmail.com>
2023-02-28 14:41:14 -0400

    Cambios en la principal

--- source/views/main_page.php
+++ source/views/main_page.php
@@ -6,7 +6,7 @@ require('nav_bar.php');
 
 <div class="home-content">
     <div class="overview-boxes">
-        <div class="box">
+        <!-- <div class="box">
             <div class="right-side">
                 <div class="box-topic">Total Order</div>
                 <div class="number">40,876</div>
@@ -38,17 +38,73 @@ require('nav_bar.php');
                 </div>
             </div>
             <i class='bx bx-cart cart three'></i>
-        </div>
+        </div> -->
         <div class="box">
+
             <div class="right-side">
-                <div class="box-topic">Total Return</div>
-                <div class="number">11,086</div>
-                <div class="indicator">
+                <div class="box-topic">Divisa</div>
+                <div class="number">BS 50,86</div>
+                <!-- <div class="indicator">
+        <i class='bx bx-down-arrow-alt down'></i>
+        <span class="text">Down From Today</span>
+    </div> -->
+            </div>
+            <div id="icon_bank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 640 512">
+                    <path fill="currentColor" d="M535 41c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l64 64c4.5 4.5 7 10.6 7 17s-2.5 12.5-7 17l-64 64c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23l-174-.2c-13.3 0-24-10.7-24-24s10.7-24 24-24h174.1L535 41zM105 377l-23 23h174c13.3 0 24 10.7 24 24s-10.7 24-24 24H81.9l23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L7 441c-4.5-4.5-7-10.6-7-17s2.5-12.5 7-17l64-64c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM96 64h241.9c-3.7 7.2-5.9 15.3-5.9 24c0 28.7 23.3 52 52 52h117.4c-4 17 .6 35.5 13.8 48.8c20.3 20.3 53.2 20.3 73.5 0l19.3-19.3V384c0 35.3-28.7 64-64 64H302.1c3.7-7.2 5.9-15.3 5.9-24c0-28.7-23.3-52-52-52H138.6c4-17-.6-35.5-13.8-48.8c-20.3-20.3-53.2-20.3-73.5 0L32 342.5V128c0-35.3 28.7-64 64-64zm64 64H96v64c35.3 0 64-28.7 64-64zm384 192c-35.3 0-64 28.7-64 64h64v-64zm-224 32a96 96 0 1 0 0-192a96 96 0 1 0 0 192z" />
+                </svg>
+            </div>
+
+        </div>
+
+        <div class="box">
+
+            <div class="right-side">
+                <div class="box-topic">G. Semanal</div>
+                <div class="number">$11,086</div>
+                <!-- <div class="indicator">
                     <i class='bx bx-down-arrow-alt down'></i>
                     <span class="text">Down From Today</span>
-                </div>
+                </div> -->
             </div>
-            <i class='bx bxs-cart-download cart four'></i>
+            <div id="icon_bank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 24 24"><g transform="rotate(180 12 12)"><path fill="currentColor" d="M17.6 18L8 8.4V17H6V5h12v2H9.4l9.6 9.6Z"/></g></svg>
+            </div>
+
+        </div>
+        <div class="box">
+
+            <div class="right-side">
+                <div class="box-topic">Capital</div>
+                <div class="number">$11,086</div>
+                <!-- <div class="indicator">
+        <i class='bx bx-down-arrow-alt down'></i>
+        <span class="text">Down From Today</span>
+    </div> -->
+            </div>
+            <div id="icon_bank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M21 9V7c0-1.65-1.35-3-3-3h-4c-.77 0-1.47.3-2 .78c-.53-.48-1.23-.78-2-.78H6C4.35 4 3 5.35 3 7v2c-1.65 0-3 1.35-3 3v5c0 1.65 1.35 3 3 3v2h2v-2h14v2h2v-2c1.65 0 3-1.35 3-3v-5c0-1.65-1.35-3-3-3m-7-3h4c.55 0 1 .45 1 1v2.78c-.61.55-1 1.34-1 2.22v2h-5V7c0-.55.45-1 1-1M5 7c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v7H6v-2c0-.88-.39-1.67-1-2.22V7m17 10c0 .55-.45 1-1 1H3c-.55 0-1-.45-1-1v-5c0-.55.45-1 1-1s1 .45 1 1v4h16v-4c0-.55.45-1 1-1s1 .45 1 1v5Z" />
+                </svg>
+            </div>
+
+        </div>
+        <div class="box">
+
+            <div class="right-side">
+                <div class="box-topic">Total</div>
+                <div class="number">$11,086</div>
+                <!-- <div class="indicator">
+                    <i class='bx bx-down-arrow-alt down'></i>
+                    <span class="text">Down From Today</span>
+                </div> -->
+            </div>
+            <div id="icon_bank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M15 10c0-.55.45-1 1-1s1 .45 1 1s-.45 1-1 1s-1-.45-1-1M8 9h5V7H8v2m14-1.5v6.97l-2.82.94L17.5 21H12v-2h-2v2H4.5S2 12.54 2 9.5S4.46 4 7.5 4h5c.91-1.21 2.36-2 4-2a1.498 1.498 0 0 1 1.38 2.08c-.14.34-.26.73-.32 1.15l2.27 2.27H22m-2 2h-1L15.5 6c0-.65.09-1.29.26-1.91c-.97.25-1.76.97-2.09 1.91H7.5C5.57 6 4 7.57 4 9.5c0 1.88 1.22 6.65 2 9.5h2v-2h6v2h2l1.56-5.15l2.44-.82V9.5Z" />
+                </svg>
+            </div>
+
         </div>
     </div>
 
@@ -235,56 +291,56 @@ require('nav_bar.php');
             <ul class="top-sales-details">
                 <li>
                     <a href="#">
-                        <img src="../images/sunglasses.jpg" alt="">
-                        <span class="product">Vuitton Sunglasses</span>
+                        <img src="../images/mastercard.jpg" alt="">
+                        <span class="product">Master Card</span>
                     </a>
                     <span class="price">$1107</span>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="../images/jeans.jpg" alt="">
-                        <span class="product">Hourglass Jeans </span>
+                        <img src="../images/bancomercantil.png" alt="">
+                        <span class="product">Banco Mercantil </span>
                     </a>
                     <span class="price">$1567</span>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="../images/nike.jpg" alt="">
-                        <span class="product">Nike Sport Shoe</span>
+                        <img src="../images/bancocentralvenezuela.png" alt="">
+                        <span class="product">Banco Central de Venezuela</span>
                     </a>
                     <span class="price">$1234</span>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="../images/scarves.jpg" alt="">
-                        <span class="product">Hermes Silk Scarves.</span>
+                        <img src="../images/paypal.png" alt="">
+                        <span class="product">Paypal</span>
                     </a>
                     <span class="price">$2312</span>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="../images/blueBag.jpg" alt="">
-                        <span class="product">Succi Ladies Bag</span>
+                        <img src="../images/banesco.png" alt="">
+                        <span class="product">Banesco</span>
                     </a>
                     <span class="price">$1456</span>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="../images/bag.jpg" alt="">
-                        <span class="product">Gucci Womens's Bags</span>
+                        <img src="../images/provincial.png" alt="">
+                        <span class="product">BBVA Provincial</span>
                     </a>
                     <span class="price">$2345</span>
                 <li>
                     <a href="#">
-                        <img src="../images/addidas.jpg" alt="">
-                        <span class="product">Addidas Running Shoe</span>
+                        <img src="../images/airtm.png" alt="">
+                        <span class="product">Airtm</span>
                     </a>
                     <span class="price">$2345</span>
                 </li>
                 <li>
                     <a href="#">
-                        <img src="../images/shirt.jpg" alt="">
-                        <span class="product">Bilack Wear's Shirt</span>
+                        <img src="../images/bancaribe.jpeg" alt="">
+                        <span class="product">Bancaribe</span>
                     </a>
                     <span class="price">$1245</span>
                 </li>
